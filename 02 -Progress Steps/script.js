9a79eafab68513bf3e35b5ad7f2824df0f40b1f2
Sara Romao <sara-romao@live.com>
2022-02-26 10:50:59 -0300

    progress steps

---
+++ 02 -Progress Steps/script.js
@@ -0,0 +1,59 @@
+
+
+const progress = document.getElementById('progress');
+const prev = document.getElementById('prev');
+const netx = document.getElementById('next');
+const circles = document.querySelectorAll('.circle');
+
+
+
+let currentActive = 1;
+
+netx.addEventListener('click', () =>{
+
+    currentActive++
+
+    if(currentActive > circles.length){
+        currentActive = circles.length
+    }
+
+    update()
+
+})
+
+prev.addEventListener('click', () =>{
+
+    currentActive--
+
+    if(currentActive < 1){
+        currentActive = 1
+    }
+
+    update()
+})
+
+function update(){
+        circles.forEach((circle, idx)=>{
+            if(idx < currentActive){
+                circle.classList.add('active')
+            }else{
+                circle.classList.remove('active')
+            }
+
+        })
+
+
+        const actives = document.querySelectorAll('.active')
+        
+        progress.style.width= ((actives.length -1) / (circles.length -1))* 100 + "%"
+
+        if(currentActive === 1){
+            prev.disabled = true
+        }else if(currentActive === circles.length){
+            netx.disabled = true
+        }else{
+            prev.disabled = false;
+            netx.disabled = false;
+        }
+
+}
